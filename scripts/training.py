0ca15ecea148aad9a8d845038103fed1547c0ff8
Arup Jana <arupjana7365@gmail.com>
2024-02-25 20:29:56 +0530

    added argparse feature

--- scripts/training.py
+++ scripts/training.py
@@ -1,40 +1,44 @@
-from keras.callbacks import ModelCheckpoint
 import os
+import argparse
 from yolov1.settings import MODEL_DIR, MODEL_WEIGHTS, EPOCHS
 from yolov1.loss import yolo_loss
 from yolov1.utils import read_json
-from yolov1.settings import TRAIN_ANNOTATIONS, VAL_ANNOTATIONS, BATCH_SIZE, DATA_DIR
+from yolov1.settings import *
 from yolov1.data import YoloDataset
 from yolov1.model import yolov1, pretrained_yolov1
 from yolov1.train import CustomLearningRateScheduler, lr_schedule
 
 if __name__=="__main__":
+    parser = argparse.ArgumentParser('help in how to train the model and set parameters.')
+    parser.add_argument('-e', '-epoch', 'set epochs for training the model')
+    parser.add_argument('-b', '--batch', 'set the batch size')
+    args = vars(parser.parse_args())
+    
+    epochs = int(args['epoch'])
+    batch_size = int(args['batch'])
+
     train_annotations = read_json(TRAIN_ANNOTATIONS)
     val_annotations = read_json(VAL_ANNOTATIONS)
 
-    train_gen = YoloDataset(train_annotations, BATCH_SIZE, DATA_DIR)
-    val_gen = YoloDataset(val_annotations, BATCH_SIZE, DATA_DIR)
+    train_gen = YoloDataset(train_annotations, batch_size, DATA_DIR)
+    val_gen = YoloDataset(val_annotations, batch_size, DATA_DIR)
 
     model = pretrained_yolov1()
 
     if not os.path.exists(MODEL_DIR):
         os.makedirs(MODEL_DIR)
 
-    mcp_save = ModelCheckpoint(os.path.join(MODEL_DIR, MODEL_WEIGHTS),
-                            save_best_only=True,
-                            monitor='val_loss',
-                            mode='min')
-
     model.compile(loss=yolo_loss, optimizer='adam')
 
     model.fit(
         x=train_gen,
-        steps_per_epoch=len(train_annotations)//BATCH_SIZE,
-        epochs=EPOCHS,
+        steps_per_epoch=len(train_annotations)//batch_size,
+        epochs=epochs,
         validation_data=val_gen,
-        validation_steps=len(val_annotations)//BATCH_SIZE,
+        validation_steps=len(val_annotations)//batch_size,
         callbacks=[
-            CustomLearningRateScheduler(lr_schedule),
-            mcp_save
+            CustomLearningRateScheduler(lr_schedule)
         ]
     )
+
+    model.save_weights(os.path.join(BASE_PATH, MODEL_DIR, MODEL_WEIGHTS))
--- scripts/yolov1/settings.py
+++ scripts/yolov1/settings.py
@@ -3,14 +3,12 @@ import os
 S = 7
 B = 2
 C = 5
-BATCH_SIZE = 16
 IMG_SIZE = (448, 448)
 DATA_DIR = '.data'
 TRAIN_ANNOTATIONS = '.data/json_annotations/train_annotations.json'
 VAL_ANNOTATIONS = '.data/json_annotations/val_annotations.json'
 MODEL_DIR = 'model'
 MODEL_WEIGHTS = 'yolov1.h5'
-EPOCHS = 2
 BASE_PATH = os.path.join(os.path.realpath(__file__), os.pardir, os.pardir, os.pardir)
 CLASS2ID = {'car': 0, 'bus': 1, 'truck': 2, 'motorbike': 3, 'pedestrian': 4}
 ID2CLASS = {0: 'car', 1: 'bus', 2: 'truck', 3: 'motorbike', 4: 'pedestrian'}
